7b642a04c445ba5b9437dc9872b6bf5e089a3a1a
JeinnertSalazarM <stevensalazsr@gmail.com>
2025-02-21 21:55:58 -0500

    Traduccion y rutas para ingles

---
+++ src/i18n/ui.ts
@@ -0,0 +1,58 @@
+export const languageList = {
+  es: "Español",
+  en: "English",
+};
+
+export const labels = {
+  es: {
+    //metatitle
+    "meta.title": "Portafolio Jeinnert Salazar",
+    //header
+    "nav.projects": "Proyectos",
+    "nav.experience": "Experiencia",
+    "nav.stack": "Tecnologías",
+    "nav.contact": "Contacto",
+    //main
+    "main.badge": "Disponible para trabajar",
+    "main.title": "Hola, soy Jeinnert",
+
+    "main.projects": "Proyectos",
+    "main.experience": "Experiencia",
+    "main.stack": "Tecnologías",
+
+    //Description
+    "desc.span1": "Desarrollador Fullstack",
+    "desc.span2":
+      "de Colombia, apasionado por crear soluciones innovadoras con diseño intuitivo y funcionalidad robusta.",
+    "desc.span3": "Especialista",
+    "desc.span4": "en tecnologías modernas y",
+    "desc.span5": "comprometido",
+    "desc.span6": "en el desarrollo de productos de alta calidad. ",
+  },
+  en: {
+    //metatitle
+    "meta.title": "Portfolio Jeinnert Salazar",
+    //header
+    "nav.projects": "Projects",
+    "nav.experience": "Experience",
+    "nav.stack": "Stack",
+    "nav.contact": "Contact",
+    //
+    //main
+    "main.badge": "Available for work",
+    "main.title": "Hi, I'm Jeinnert",
+
+    "main.projects": "Projects",
+    "main.experience": "Experience",
+    "main.stack": "Stack",
+
+    //Description
+    "desc.span1": "Fullstack Developer",
+    "desc.span2":
+      "from Colombia, passionate about creating innovative solutions with intuitive design and robust functionality.",
+    "desc.span3": "Especialist",
+    "desc.span4": "in modern technologies and ",
+    "desc.span5": " engaged",
+    "desc.span6": "in the development of high-quality products. ",
+  },
+};
